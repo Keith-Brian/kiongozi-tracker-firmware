2aba7e23073af98e9ff540f14ba4f57344b410a1
keith-brian <chiefdeveloper254@gmail.com>
2025-12-28 19:14:30 +0300

    serialize json response

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,17 @@
 
 // function prototypes
 void connectToWiFi();
-void createJsonDoc (char*, size_t size);
+void createJsonDoc(char *, size_t size);
+void sendSimpleGetRequest();
+void deSerializeJsonResponse(const String Json);
 
 // creating objects for WiFi and HTTPClient
 HTTPClient http;
 
-void setup() {
+String response = "";
+
+void setup()
+{
   Serial.begin(115200);
   connectToWiFi();
   // create a JSON buffer
@@ -25,47 +30,79 @@ void setup() {
   Serial.println(payLoad);
 
   // Example of sending HTTP GET request
+  sendSimpleGetRequest();
+  deSerializeJsonResponse(response);
+}
+
+void loop()
+{
+}
+
+// simple GET request example
+void sendSimpleGetRequest()
+{
   http.begin("http://jsonplaceholder.typicode.com/todos/1");
   int httpResponseCode = http.GET();
   Serial.print("HTTP Response code: ");
   Serial.println(httpResponseCode);
 
   // get the response payload
-  if (httpResponseCode > 0) { 
-    String response = http.getString();
+  if (httpResponseCode > 0)
+  {
+    response = http.getString();
     Serial.println("Response payload:");
     Serial.println(response);
-  } else {
+  }
+  else
+  {
     Serial.print("Error on HTTP request: ");
     Serial.println(httpResponseCode);
   }
   http.end(); // free resources
-
 }
 
-void loop() {
- 
+// Simple JSON deSerialization from the request
+void deSerializeJsonResponse(const String Json)
+{
+  JsonDocument doc;
+  DeserializationError error = deserializeJson(doc, Json);
+
+  if (error)
+  {
+    Serial.print("deserializeJson() failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+  const char *title = doc["title"];
+  bool completed = doc["completed"];
+
+  Serial.print("Title: ");
+  Serial.println(title);
+  Serial.print("Completed: ");
+ // Serial.println(completed ? "true" : "false");
 }
 
-// creating a json document 
-void createJsonDoc (char* payLoad, size_t size) {
+
+// creating a json document
+void createJsonDoc(char *payLoad, size_t size)
+{
   JsonDocument doc;
 
   doc["email"] = "devkaybee@gmail.com";
   doc["password"] = "d3kaybee";
 
   serializeJsonPretty(doc, payLoad, size);
-  
-  //to know exact size of json to be allocated
+
+  // to know exact size of json to be allocated
   size_t jsonSize = measureJson(doc);
-  Serial.print("Measured JSON size: ");  
+  Serial.print("Measured JSON size: ");
   Serial.println(jsonSize);
 }
 
-
-
 // configure WiFi connection using credentials from secrets.h
-void connectToWiFi() {
+void connectToWiFi()
+{
   Serial.print("Connecting to WiFi...");
   Serial.println(WIFI_SSID);
 
@@ -74,18 +111,22 @@ void connectToWiFi() {
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  while (WiFi.status() !=WL_CONNECTED && wifiConnectAttempts < maxAttempts) {
+  while (WiFi.status() != WL_CONNECTED && wifiConnectAttempts < maxAttempts)
+  {
     delay(500);
     Serial.print(".");
     wifiConnectAttempts++;
   }
 
   // Check if connected
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     Serial.println("\nWiFi connected successfully!");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
-  } else {
+  }
+  else
+  {
     Serial.println("\nFailed to connect to WiFi.");
   }
 }
