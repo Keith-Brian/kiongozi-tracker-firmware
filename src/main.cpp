c3165c36f8d8048556e65f145cebe38000587093
keith-brian <chiefdeveloper254@gmail.com>
2025-12-28 14:08:32 +0300

    initiate WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
+#include "secrets.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// include other necessary libraries here
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+// function prototypes
+void connectToWiFi();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  connectToWiFi();
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+// configure WiFi connection using credentials from secrets.h
+void connectToWiFi() {
+  Serial.print("Connecting to WiFi...");
+  Serial.println(WIFI_SSID);
+
+  int wifiConnectAttempts = 0;
+  const int maxAttempts = 20;
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() !=WL_CONNECTED && wifiConnectAttempts < maxAttempts) {
+    delay(500);
+    Serial.print(".");
+    wifiConnectAttempts++;
+  }
+
+  // Check if connected
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nWiFi connected successfully!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nFailed to connect to WiFi.");
+  }
 }
